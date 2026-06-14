58b9d255628df5366eaff68ca1112a2f3ae304bc
Daksh <bhardwajdakshcodes@gmail.com>
2026-06-14 23:01:59 +0530

    Redirect build output path to root directory for Vercel auto-detection

--- config-overrides.js
+++ config-overrides.js
@@ -6,7 +6,7 @@ module.exports = {
     paths.appSrc = path.resolve(__dirname, 'frontend/skillforge-frontend/src');
     paths.appPublic = path.resolve(__dirname, 'frontend/skillforge-frontend/public');
     paths.appIndexJs = path.resolve(__dirname, 'frontend/skillforge-frontend/src/index.js');
-    paths.appBuild = path.resolve(__dirname, 'frontend/skillforge-frontend/build');
+    paths.appBuild = path.resolve(__dirname, 'build');
     paths.appHtml = path.resolve(__dirname, 'frontend/skillforge-frontend/public/index.html');
     paths.appPackageJson = path.resolve(__dirname, 'package.json'); // Use root package.json
     return paths;
